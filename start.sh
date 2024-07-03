21e003e1d450071329a6cb0ad942de5f938c4635
joetester1965 at maildotcom <24988751+JoeTester1965@users.noreply.github.com>
2024-07-03 22:18:07 +0100

    So can see errorz

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 nohup ./stop.sh 2>/dev/null
-nohup python3 zmqpubsink.py 2>/dev/null &
-nohup python3 SDRPlayBatDetector.py 2>/dev/null &
+nohup python3 zmqpubsink.py  &> zmqpubsink.log &
+nohup python3 SDRPlayBatDetector.py &> SDRPlayBatDetector.log  &
