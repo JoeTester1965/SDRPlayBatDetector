747509b768d4a0ab79c36c8cc25fbb356663c23e
pi <pi@shed>
2024-07-04 22:36:03 +0100

    testing

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 nohup ./stop.sh 2>/dev/null
-nohup python3 zmqpubsink.py  &> zmqpubsink.log &
 nohup python3 SDRPlayBatDetector.py &> SDRPlayBatDetector.log  &
+nohup python3 zmqpubsink.py  &> zmqpubsink.log &
+
